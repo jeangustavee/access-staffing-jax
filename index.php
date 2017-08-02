fdf321be2aba5d7f02e0341ef6967777342f3104
Kenold Beauplan <bkenold@gmail.com>
2017-08-02 13:45:06 -0400

    add index section

--- index.php
+++ index.php
@@ -17,40 +17,184 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
+            <section class="engine"><a href="#">bootstrap button</a></section><section class="cid-qqXEjGRE0S mbr-fullscreen mbr-parallax-background" id="header2-2" data-rv-view="1283">
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
 
-			<?php
-			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+                <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(35, 35, 35);"></div>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+                <div class="container align-center">
+                    <div class="media-container-column mbr-white col-md-10 offset-md-1">
+                        <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">Access International</h1>
 
-			endwhile;
+                        <p class="mbr-text pb-3 mbr-fonts-style display-2">
+                            Your temporary staffing solution provider</p>
+                        <div class="mbr-section-btn">
+                            <a class="btn btn-md btn-secondary display-4" href="employer.html">EMPLOYERS</a>
+                            <a class="btn btn-md btn-white-outline display-4" href="jobs.html">JOB SEEKERS</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+                    <a href="#next">
+                        <i class="mbri-down mbr-iconfont"></i>
+                    </a>
+                </div>
+            </section>
 
-			the_posts_navigation();
+            <section class="engine"><a href="#">bootstrap button</a></section><section class="cid-qqXEjGRE0S mbr-fullscreen mbr-parallax-background" id="header2-2" data-rv-view="1283">
 
-		else :
 
-			get_template_part( 'template-parts/content', 'none' );
 
-		endif; ?>
+                <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(35, 35, 35);"></div>
+
+                <div class="container align-center">
+                    <div class="media-container-column mbr-white col-md-10 offset-md-1">
+                        <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">Access International</h1>
+
+                        <p class="mbr-text pb-3 mbr-fonts-style display-2">
+                            Your temporary staffing solution provider</p>
+                        <div class="mbr-section-btn">
+                            <a class="btn btn-md btn-secondary display-4" href="employer.html">EMPLOYERS</a>
+                            <a class="btn btn-md btn-white-outline display-4" href="jobs.html">JOB SEEKERS</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+                    <a href="#next">
+                        <i class="mbri-down mbr-iconfont"></i>
+                    </a>
+                </div>
+            </section>
+
+            <section class="mbr-section content4 cid-qqXNL4116z" id="content4-3" data-rv-view="1286">
+
+
+
+                <div class="container">
+                    <div class="media-container-row">
+                        <div class="title col-12 col-md-8">
+                            <h2 class="align-center pb-3 mbr-fonts-style display-2">
+                                A company that cares for you:</h2>
+                            <h3 class="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-5"><div><span style="font-size: 1.5rem;">ACCESS INETRNATIONAL LLC was established as a LLC with the expectation of rapid expansion in staffing for medical, hospitality and office industry.</span><br></div></h3>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="features2 cid-qqXYb2Opis mbr-parallax-background" id="features2-6" data-rv-view="1288">
+
+
+
+                <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(239, 239, 239);">
+                </div>
+
+                <div class="container">
+                    <div class="media-container-row">
+                        <div class="card p-3 col-12 col-md-6 col-lg-3">
+                            <div class="card-wrapper">
+                                <div class="card-img">
+                                    <img src="assets/images/medical-1080x720.jpg" alt="" title="" media-simple="true">
+                                </div>
+                                <div class="card-box">
+                                    <h4 class="card-title pb-3 mbr-fonts-style display-5">
+                                        MEDICAL</h4>
+                                    <p class="mbr-text mbr-fonts-style display-7">We find employment for Nurses, CNAs, Therapists, Pharmacists, Medical Billing Receptionists, and many other healthcare professionals.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card p-3 col-12 col-md-6 col-lg-3">
+                            <div class="card-wrapper">
+                                <div class="card-img">
+                                    <img src="assets/images/hospitality-1080x720.jpg" alt="" title="" media-simple="true">
+                                </div>
+                                <div class="card-box ">
+                                    <h4 class="card-title pb-3 mbr-fonts-style display-5">
+                                        HOSPITALITY</h4>
+                                    <p class="mbr-text mbr-fonts-style display-7">We collaborate with companies in the hospitlity industry to bring you some of the best positions: Housekeeping, Food Service, Stewarding, Janitorial, etc.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card p-3 col-12 col-md-6 col-lg-3">
+                            <div class="card-wrapper">
+                                <div class="card-img">
+                                    <img src="assets/images/customer-sercoe-1080x720.jpg" alt="" title="" media-simple="true">
+                                </div>
+                                <div class="card-box">
+                                    <h4 class="card-title pb-3 mbr-fonts-style display-5">
+                                        CUSTOMER SERVICES</h4>
+                                    <p class="mbr-text mbr-fonts-style display-7">Altogether we keep customers happy. We will match the best professionals to the right jobs. A few of our fields are Inbound/outbound Customer Care Specialist, Help Desk Tech, etc.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card p-3 col-12 col-md-6 col-lg-3">
+                            <div class="card-wrapper">
+                                <div class="card-img">
+                                    <img src="assets/images/office-industry-1080x720.jpg" alt="" title="" media-simple="true">
+                                </div>
+                                <div class="card-box">
+                                    <h4 class="card-title pb-3 mbr-fonts-style display-5">
+                                        OFFICE INDUSTRY</h4>
+                                    <p class="mbr-text mbr-fonts-style display-7">With nearly 1.3 million jobs set to be created in this field by 2022, those with office, retail, and hospitality skills are clearly in high demand! We are here to help you find your next career.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="mbr-section info3 cid-qqYeogFKCE" id="info3-i" data-rv-view="1291">
+
+
+
+
+
+                <div class="container">
+                    <div class="media-container-column title col-12 col-md-10 offset-md-1">
+                        <h2 class="align-left mbr-bold mbr-white pb-3 mbr-fonts-style display-1">
+                            FOR JOB SEEKERS</h2>
+                        <h3 class="mbr-section-subtitle align-left mbr-light mbr-white pb-3 mbr-fonts-style display-2">
+                            Take a look at our job listings</h3>
+                        <p class="mbr-text align-left mbr-white mbr-fonts-style display-5">
+                            ACCESS INTERNATIONAL makes the process to find a job simple so the focus is always on our candidates. We got the right job for you!
+                        </p>
+                        <div class="mbr-section-btn align-left py-4">
+                            <a class="btn btn-primary display-4" href="jobs.html">SEE JOB LISTINGS</a>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="mbr-section info4 cid-qqYf8MXOqS" id="info4-j" data-rv-view="1294">
+
+
+
+
+                <div class="container">
+                    <div class="media-container-column title col-12 col-md-10 offset-md-1">
+                        <h2 class="align-right mbr-bold mbr-white pb-3 mbr-fonts-style display-1">
+                            FOR EMPLOYERS</h2>
+                        <h3 class="mbr-section-subtitle align-right mbr-light mbr-white pb-3 mbr-fonts-style display-2">
+                            Your business needs</h3>
+                        <p class="mbr-text align-right mbr-white mbr-fonts-style display-5">
+                            We harness the power of technology to focus on your business needs. At Access International we know what you want and we provide exactly what you want.
+                        </p>
+                        <div class="mbr-section-btn align-right py-4">
+
+                            <a class="btn btn-white-outline display-4" href="employer.html">LEARN MORE</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
