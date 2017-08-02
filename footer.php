a5dfedad0eb3f4928753205d2c9cd491225a73a6
Kenold Beauplan <bkenold@gmail.com>
2017-08-02 13:44:56 -0400

    add footer section

--- footer.php
+++ footer.php
@@ -14,17 +14,73 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'access-staffing-jax' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'access-staffing-jax' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'access-staffing-jax' ), 'access-staffing-jax', '<a href="http://www.anteanstudios.com/">Antean Studios</a>' );
-			?>
-		</div><!-- .site-info -->
+        <section class="cid-qqYg8JwYYP mbr-reveal" id="footer2-n" data-rv-view="1297">
+
+
+
+
+
+            <div class="container">
+                <div class="media-container-row content mbr-white">
+                    <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                        <p class="mbr-text">
+                            <strong>Address</strong>
+                            <br>101 Century 21 Dr. Ste 115<br>Jacksonville, FL 32216<br>
+                            <br><strong>Contacts</strong>
+                            <br>Email: tlugo@accessinternationaljax.com
+                            <br>Phone: (904) 248-9569
+                            <br>Support:&nbsp;support@accessinternationaljax.com</p>
+                    </div>
+                    <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                        <p class="mbr-text">
+                            <strong>Links</strong>
+                            <br><a href="about.html" class="text-success">About us</a><br><a href="jobs.html" class="text-success">Jobs</a>
+                            <br><a href="employer.html" class="text-success">Employer</a><br>
+                            <br><strong>Feedback</strong>
+                            <br>Please send us your ideas, bug reports, suggestions! Any feedback would be appreciated.
+                        </p>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <div class="google-map"><iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0Dx_boXQiwvdz8sJHoYeZNVTdoWONYkU&amp;q=place_id:EjMxMDEgQ2VudHVyeSAyMSBEciAjMTE1LCBKYWNrc29udmlsbGUsIEZMIDMyMjE2LCBVU0E" allowfullscreen=""></iframe></div>
+                    </div>
+                </div>
+                <div class="footer-lower">
+                    <div class="media-container-row">
+                        <div class="col-sm-12">
+                            <hr>
+                        </div>
+                    </div>
+                    <div class="media-container-row mbr-white">
+                        <div class="col-sm-6 copyright">
+                            <p class="mbr-text mbr-fonts-style display-7">
+                                © Copyright 2017 ACCESS INTERNATIONAL - All Rights Reserved. Website by <a href="http://anteanstudios.com" target="_blank" class="text-success">ANTEAN STUDIOS</a>&nbsp;904-580-5923</p>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="social-list align-right">
+                                <div class="soc-item">
+                                    <a href="https://twitter.com/" target="_blank">
+                                        <span class="mbr-iconfont mbr-iconfont-social socicon-twitter socicon" media-simple="true"></span>
+                                    </a>
+                                </div>
+                                <div class="soc-item">
+                                    <a href="#" target="_blank">
+                                        <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon" media-simple="true"></span>
+                                    </a>
+                                </div>
+                                <div class="soc-item">
+                                    <a href="#" target="_blank">
+                                        <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon" media-simple="true"></span>
+                                    </a>
+                                </div>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
