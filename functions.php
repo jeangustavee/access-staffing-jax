5338ca48f434096cf091fe0cf55da620e587dd0c
Kenold Beauplan <bkenold@gmail.com>
2017-08-02 14:15:56 -0400

    move JS scripts to footer

--- functions.php
+++ functions.php
@@ -114,6 +114,17 @@ function access_staffing_jax_widgets_init() {
 add_action( 'widgets_init', 'access_staffing_jax_widgets_init' );
 
 /**
+ * Add Google Fonts
+ */
+function wpb_add_google_fonts() {
+
+    wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i,700,700i,900,900i', false );
+}
+
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
+
+/**
  * Enqueue scripts and styles.
  */
 function access_staffing_jax_scripts() {
@@ -127,19 +138,19 @@ function access_staffing_jax_scripts() {
     wp_enqueue_style( 'access-staffing-jax-global', get_template_directory_uri() . '/css/mbr-additional.css');
 
 
-    wp_enqueue_script( 'access-staffing-jax-jquery', get_template_directory_uri() . '/js/jquery.min.js');
+    wp_enqueue_script( 'access-staffing-jax-jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), '', true);
     wp_enqueue_script( 'access-staffing-jax-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'access-staffing-jax-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'access-staffing-jax-tether', get_template_directory_uri() . '/js/tether.min.js');
-	wp_enqueue_script( 'access-staffing-jax-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js');
-	wp_enqueue_script( 'access-staffing-jax-smoothscroll', get_template_directory_uri() . '/js/smooth-scroll.js');
-	wp_enqueue_script( 'access-staffing-jax-touch', get_template_directory_uri() . '/js/jquery.touch-swipe.min.js');
-	wp_enqueue_script( 'access-staffing-jax-viewport', get_template_directory_uri() . '/js/jquery.viewportchecker.js');
-	wp_enqueue_script( 'access-staffing-jax-jarallax', get_template_directory_uri() . '/js/jarallax.min.js');
-	wp_enqueue_script( 'access-staffing-jax-scriptmin', get_template_directory_uri() . '/js/script.min.js');
-	wp_enqueue_script( 'access-staffing-jax-script', get_template_directory_uri() . '/js/script.js');
+	wp_enqueue_script( 'access-staffing-jax-tether', get_template_directory_uri() . '/js/tether.min.js', array(), '', true);
+	wp_enqueue_script( 'access-staffing-jax-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '', true);
+	wp_enqueue_script( 'access-staffing-jax-smoothscroll', get_template_directory_uri() . '/js/smooth-scroll.js', array(), '', true);
+	wp_enqueue_script( 'access-staffing-jax-touch', get_template_directory_uri() . '/js/jquery.touch-swipe.min.js', array(), '', true);
+	wp_enqueue_script( 'access-staffing-jax-viewport', get_template_directory_uri() . '/js/jquery.viewportchecker.js', array(), '', true);
+	wp_enqueue_script( 'access-staffing-jax-jarallax', get_template_directory_uri() . '/js/jarallax.min.js', array(), '', true);
+	wp_enqueue_script( 'access-staffing-jax-scriptmin', get_template_directory_uri() . '/js/script.min.js', array(), '', true);
+	wp_enqueue_script( 'access-staffing-jax-script', get_template_directory_uri() . '/js/script.js', array(), '', true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
